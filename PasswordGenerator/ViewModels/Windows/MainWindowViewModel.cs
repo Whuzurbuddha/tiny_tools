f65a4a39133800a4e4616ef77346981920359c00
Alexander Päplow <alexander@paeplow.info>
2024-02-25 13:36:56 +0100

    update

---
+++ PasswordGenerator/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System.Collections.ObjectModel;
+using Wpf.Ui.Controls;
+
+namespace PasswordGenerator.ViewModels.Windows
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _applicationTitle = "WPF UI - PasswordGenerator";
+
+        [ObservableProperty]
+        private ObservableCollection<object> _menuItems = new()
+        {
+            new NavigationViewItem()
+            {
+                Content = "New",
+                Icon = new SymbolIcon {Symbol = SymbolRegular.Password20},
+                TargetPageType = typeof(Views.Pages.Password)
+            }
+        };
+
+        [ObservableProperty]
+        private ObservableCollection<object> _footerMenuItems = new()
+        {
+            new NavigationViewItem()
+            {
+                Content = "Settings",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Settings24 },
+                TargetPageType = typeof(Views.Pages.SettingsPage)
+            }
+        };
+
+        [ObservableProperty]
+        private ObservableCollection<MenuItem> _trayMenuItems = new()
+        {
+            new MenuItem { Header = "Home", Tag = "tray_home" }
+        };
+    }
+}
---
+++ PasswordGenerator/Views/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace PasswordGenerator.Views.Pages;
+
+public partial class DashboardPage : Page
+{
+    public DashboardPage()
+    {
+        InitializeComponent();
+    }
+}
